3f6107413e802d57fadccf74e159f0bff59b7e6f
noor <noorjahan1996@gmail.com>
2024-08-22 10:55:21 +0530

    test

---
+++ src/main/java/com/example/simpleSBApplication/SimpleSbApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleSBApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simpleSBApplication/SimpleSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleSBApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
