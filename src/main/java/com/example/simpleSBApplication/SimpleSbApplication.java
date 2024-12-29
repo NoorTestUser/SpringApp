2ae81e912edc12ba8db68d8a02149731bbaed815
noor <noorjahan1996@gmail.com>
2024-12-29 15:31:27 +0530

    first commit

--- src/main/java/com/example/simpleSBApplication/SimpleSbApplication.java
+++ src/main/java/com/example/simpleSBApplication/SimpleSbApplication.java
@@ -1,12 +1,18 @@
 package com.example.simpleSBApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SimpleSbApplication {
 
+	public static Logger LOG = LoggerFactory.getLogger(SimpleSbApplication.class);
+
 	public static void main(String[] args) {
+
+		LOG.info("Application started");
 		SpringApplication.run(SimpleSbApplication.class, args);
 	}
 
--- src/test/java/com/example/simpleSBApplication/SimpleSbApplicationTests.java
+++ src/test/java/com/example/simpleSBApplication/SimpleSbApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.simpleSBApplication;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SimpleSbApplicationTests {
+	public static Logger LOG = LoggerFactory.getLogger(SimpleSbApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void test(){
+		LOG.info("Test Successful");
 	}
 
 }
